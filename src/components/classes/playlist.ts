1c4c7e45007a17528f8b028b31d0e5ea1d2e1d93
Alex Kozik <sous3-Bryson>
2021-08-27 03:17:58 -0400

    Added a couple of new functions and classes. They aren't in use yet. These are for extracting songs from spotify playlists and searching for spotify playlists.

---
+++ src/components/classes/playlist.ts
@@ -0,0 +1,10 @@
+export default class playlist {
+    public url: string
+    public desc: string
+    
+  constructor(url: string, desc: string) {
+    this.url = url
+    this.desc = desc
+  }
+
+}
--- testing/test.js
+++ testing/test.js
@@ -1,36 +1,57 @@
+// searching for spotify playlists using keywords
+
+
+let SpotifyWebApi = require('spotify-web-api-node');
+const token = 'BQBh5pDXgxvt3sMj0fcnP-YzxKqp7IYylezTdyvfE5vGdbDwVKVwSHqJQ-FcR9XoO_Tm3rbl6JFluVhn9Qh_E_0j1sUAzBQgvtMuWMgWyfvDm-GT9bliZmXr-zPlkqEY5el5C8SlNMRKmVJ3prj5MK9wk0DDr30'
+const spotifyApi = new SpotifyWebApi(
+  {
+    clientId: '9f710a7f6cf944b48396a1205b05814f',
+    clientSecret: '2a18b428a71d49b393a5d9f4365dc51a',
+    accessToken: token
+
+  }
+)
+
+const link = 'https://open.spotify.com/playlist/1Gi8ggdgM5RaQj9F7nVL7g?si=0def70c6e4744248'
+
+
+
+async function getSpotifyPlaylistsByKeywords(keywords) {
+  const data = await spotifyApi.searchPlaylists(keywords)
+  const items = data.body.playlists.items
+  let playlists = []
+
+  if (items.length == 0) return null
+  else {
+    for (let i = 0; i < items.length; i++) {
+      const url = items[i].external_urls.spotify
+      const desc = items[i].description
+      playlists.push(new playlist(url, desc))
+    }
+  }
+  return playlists
+}
+
+class playlist {
+  /**
+   * 
+   * @param {string} url 
+   * @param {string} desc 
+   */
+  constructor(url, desc) {
+    this.url = url
+    this.desc = desc
+  }
+
+}
+
 /*
-Returns a promise for an object
-the object has a property called items, which is an array of objects
-each object within that array represents a search result, each having an id.
+getSpotifyPlaylistsByKeywords("edm")
+.then(playlists => {console.log(playlists)})
 */
 
-const disbut = require('discord-buttons')
-const discord = require('discord.js')
-const { MessageButton } = require('discord.js-buttons')
 
-const client = new discord.Client()
+spotifyApi.getPlaylistTracks('2e3dcRuo9uDH6qD3NOGKAL')
+.then(tracks => console.log(tracks.body.items[0].track.external_urls.spotify))
 
-client.on('message', async message => {
-    if (message.content == 'test') {
-        const button = new MessageButton()
-        button.setStyle('blurple')
-        button.setLabel('skip')
-        button.setID('skip')
-        message.channel.send('button', {
-            buttons: [
-                button
-            ]
-        })
-    }  
-})
-
-
-client.on('clickButton', async button => {
-    if (button.id == 'skip') {
-        button.channel.send('hi')
-        button.reply.defer()
-    }
-})
-
-client.login('ODI2MjQ3MTYwNDQ2MDU4NTA3.YGJsoQ.mykagyy_X6_xh5QpXrJbHmwn4Z0')
-disbut(client)
+spotifyApi.getPlaylist()
