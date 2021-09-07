9be70a1b6153259687c6b5ddb975ae71d1259a3b
Alex Kozik <sous3-Bryson>
2021-09-07 13:59:43 -0400

    Removed some files that technically aren't part of the src.

--- testing/test2.js
+++ testing/test2.js
@@ -1,6 +1,1 @@
-const spotify = require('spotify-url-info')
-
-spotify.getData('https://open.spotify.com/playlist/1Gi8ggdgM5RaQj9F7nVL7g?si=0def70c6e4744248')
-.then(data => {
-    for (let i = 0; i<data.tracks.items.length;i++) console.log(data.tracks.items[i].track.external_urls.spotify)
-})
+const discord = require('spdl-core')
