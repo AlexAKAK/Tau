3cd7dc643148b652a073af25c7ba031c2ad2e842
Alex Kozik <sous3-Bryson>
2021-09-07 14:58:04 -0400

    Changed the run file from c to python.

---
+++ run.py
@@ -0,0 +1,27 @@
+import os
+import sys
+
+os.system('cls')
+print("\033[0;32m")
+if not os.path.isdir('node_modules'):
+    print('[Installing node_modules]')
+    os.system('npm install')
+
+if (not os.path.exists('config.json')):
+    config = open('config.json', 'w+')
+    config.write('''
+    {
+    "token": "",
+    "prefix": "t!"
+    }
+    ''')
+    print("config.json must be filled out")
+    config.close()
+    sys.exit(1)
+
+
+print('[Compiling src to out]')
+os.system('tsc --outdir out')
+
+print('[Executing index.js]')
+os.system('node out/index.js')
